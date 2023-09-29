b8fdb95c49e3562507eb081b45796e2dcd5b696a
BappySheikh1 <sheikhbappy91gmail.com>
2023-09-29 11:09:04 +0600

    login page added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,66 @@
-import { Col, Row } from "antd";
+"use client";
+import { Button, Col, Row } from "antd";
+import loginImage from "@/assets/Privacy policy-rafiki.png";
+import Image from "next/image";
+import Form from "@/components/Forms/Form";
+import FormInput from "@/components/Forms/FormInput";
+import { SubmitHandler } from "react-hook-form";
 
+type FormValues = {
+  id: string;
+  password: string;
+};
 const LoginPage = () => {
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    try {
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <Row>
-      <Col sm={12} md={16} lg={18}>
-        Col
+    <Row
+      justify="center"
+      align="middle"
+      style={{
+        minHeight: "100vh",
+      }}
+    >
+      <Col sm={12} md={16} lg={10}>
+        <Image src={loginImage} width={500} alt="login image" />
       </Col>
       <Col sm={12} md={8} lg={8}>
-        Col
+        <h1
+          style={{
+            margin: "15px 0px",
+          }}
+        >
+          First login your account
+        </h1>
+        <div>
+          <Form submitHandler={onSubmit}>
+            <div>
+              <FormInput name="id" type="text" size="large" label="User Id" />
+            </div>
+            <div
+              style={{
+                margin: "15px 0px",
+              }}
+            >
+              <FormInput
+                name="password"
+                type="password"
+                size="large"
+                label="User Password"
+              />
+            </div>
+
+            <Button type="primary" htmlType="submit">
+              Login{" "}
+            </Button>
+          </Form>
+        </div>
       </Col>
     </Row>
   );
