172587d8f94ee82b594d9cc739d21683a7e171a1
BappySheikh1 <sheikhbappy91gmail.com>
2023-10-08 05:56:54 +0800

    department table added

--- src/app/(withlayout)/super_admin/department/page.tsx
+++ src/app/(withlayout)/super_admin/department/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import ActionBar from "@/components/ui/ActionBar";
 import UMTable from "@/components/ui/UMTable";
 import { Button } from "antd";
@@ -5,15 +6,70 @@ import Link from "next/link";
 import React from "react";
 
 const ManageDepartmentPage = () => {
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+      //   sorter: true,
+      sorter: (a: any, b: any) => a.age - b.age,
+    },
+    {
+      title: "Action",
+      render: function (data: any) {
+        return (
+          <Button onClick={() => console.log(data)} type="primary" danger>
+            x
+          </Button>
+        );
+      },
+    },
+  ];
+
+  const tableData = [
+    {
+      key: "1",
+      name: "John Brown",
+      age: 32,
+    },
+    {
+      key: "2",
+      name: "Jim Green",
+      age: 42,
+    },
+  ];
+
+  const onPaginationChange = (page: number, pageSize: number) => {
+    // console.log("Page:", page, "PageSize:", pageSize);
+  };
+  const onTableChange = (pagination: any, filter: any, sorter: any) => {
+    console.log(pagination, filter, sorter);
+  };
+
   return (
     <div>
       <ActionBar title="Department List">
         <Link href={`/super_admin/department/create`}>
           <Button>Create Department</Button>
         </Link>
-
       </ActionBar>
-        <UMTable />
+      <UMTable
+        loading ={false}
+        columns={columns}
+        dataSource={tableData}
+        pageSize={5}
+        totalPages={10}
+        showSizeChanger={true}
+        onPaginationChange={onPaginationChange}
+        onTableChange={onTableChange}
+        showPagination={true}
+      />
     </div>
   );
 };
--- src/components/ui/UMTable.tsx
+++ src/components/ui/UMTable.tsx
@@ -1,65 +1,45 @@
 "use client";
-import { Button, Table } from "antd";
 
-const UMTable = () => {
-  const columns = [
-    {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
-    },
-    {
-      title: "Age",
-      dataIndex: "age",
-      key: "age",
-      //   sorter: true,
-      sorter: (a: any, b: any) => a.age - b.age,
-    },
-    {
-      title: "Action",
-      render: function (data: any) {
-        return (
-          <Button onClick={() => console.log(data)} type="primary" danger>
-            x
-          </Button>
-        );
-      },
-    },
-  ];
-  const tableData = [
-    {
-      key: "1",
-      name: "John Brown",
-      age: 32,
-    },
-    {
-      key: "2",
-      name: "Jim Green",
-      age: 42,
-    },
-  ];
+import { Table } from "antd";
 
-  const onPaginationChange = (page: number, pageSize: number) => {
-    // console.log("Page:", page, "PageSize:", pageSize);
-  };
+type UMTableProps = {
+  loading?: boolean;
+  columns: any;
+  dataSource: any;
+  pageSize?: number;
+  totalPages?: number;
+  showSizeChanger?: boolean;
+  onPaginationChange?: (page: number, pageSize: number) => void;
+  onTableChange?: (pagination: any, filter: any, sorter: any) => void;
+  showPagination?: boolean;
+};
 
-  const paginationConfig = {
-    pageSize: 5,
-    total: 10,
-    pageSizeOptions: [5, 10, 20],
-    showSizeChanger: true,
-    onChange: onPaginationChange,
-  };
-
-  const onTableChange = (pagination: any, filter: any, sorter: any) => {
-    const { order, field } = sorter;
-    console.log(order, field);
-  };
+const UMTable = ({
+  loading = false,
+  columns,
+  dataSource,
+  pageSize,
+  totalPages,
+  showSizeChanger = true,
+  onPaginationChange,
+  onTableChange,
+  showPagination = true,
+}: UMTableProps) => {
+  const paginationConfig = showPagination
+    ? {
+        pageSize: pageSize,
+        total: totalPages,
+        pageSizeOptions: [5, 10, 20],
+        showSizeChanger: showSizeChanger,
+        onChange: onPaginationChange,
+      }
+    : false;
 
   return (
     <Table
+      loading={loading}
       columns={columns}
-      dataSource={tableData}
+      dataSource={dataSource}
       pagination={paginationConfig}
       onChange={onTableChange}
     />
