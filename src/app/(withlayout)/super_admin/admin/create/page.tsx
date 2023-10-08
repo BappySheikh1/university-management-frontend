ddc11ab27a18622fed6f85c5844b06523f4fe023
BappySheikh1 <sheikhbappy91gmail.com>
2023-10-08 15:29:44 +0800

    image uploaded

--- src/app/(withlayout)/super_admin/admin/create/page.tsx
+++ src/app/(withlayout)/super_admin/admin/create/page.tsx
@@ -171,7 +171,7 @@ const CreateAdminPage = () => {
                   marginBottom: "10px",
                 }}
               >
-                <UploadImage />
+                <UploadImage name="file" />
               </Col>
             </Row>
           </div>
